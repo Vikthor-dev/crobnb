6797a5737a3ce69f3544a13c90aed98240309d78
Vikthor-dev <57874994+Vikthor-dev@users.noreply.github.com>
2025-02-28 16:01:04 +0100

    Date update

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -1,14 +1,38 @@
 import React from "react";
-import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import DateIcon from "../assets/date.svg";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateRangeCalendar } from '@mui/x-date-pickers-pro/DateRangeCalendar';
+
 function Date() {
   const [lokacija, setLokacija] = React.useState("Odaberi");
+  const [isOpened, setCalendar] = React.useState(false);
 
   const handleChange = (event: SelectChangeEvent<string>) => {
     setLokacija(event.target.value as string);
   };
+
+  const toggleCalendar = () => {
+    setCalendar(!isOpened);
+  };
+
+  function showHideCalendar() {
+    if (isOpened) {
+      return (
+        <div>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateRangeCalendar calendars={1} />
+          </LocalizationProvider>
+        </div>
+      );
+    }else{
+        return (
+            <div></div>
+        );
+    }
+  }
 
   function renderSelectedValue(selected: string) {
     if (selected === "Odaberi") {
@@ -22,35 +46,13 @@ function Date() {
             height: 30,
           }}
         >
-          <img
-            src={DateIcon}
-            alt="Datum"
-            style={{ width: 24, height: 24 }}
-          />
+          <img src={DateIcon} alt="Datum" style={{ width: 24, height: 24 }} />
           Odaberi datum
         </div>
       );
     }
-
-    return (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          gap: 12,
-          height: 30,
-        }}
-      >
-        <img
-          src={DateIcon}
-          alt="Datum"
-          style={{ width: 24, height: 24 }}
-        />
-        {selected}
-      </div>
-    );
   }
+
   return (
     <div>
       <FormControl fullWidth size="small">
@@ -59,11 +61,10 @@ function Date() {
           onChange={handleChange}
           displayEmpty
           renderValue={renderSelectedValue}
-        >
-          <MenuItem>
-          
-          </MenuItem>
-        </Select>
+          readOnly
+          onClick={toggleCalendar}
+        ></Select>
+        {showHideCalendar()}
       </FormControl>
     </div>
   );
