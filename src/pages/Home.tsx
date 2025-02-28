fb5ddbb0ea437a5b09680c5802d47c42e94e3b03
Vikthor-dev <57874994+Vikthor-dev@users.noreply.github.com>
2025-02-28 22:50:14 +0100

    Update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,8 @@ import Location from "../components/Location";
 import Accomodation from "../components/Accomodation";
 import Date from "../components/Date";
 import Guests from "../components/Guests";
+import Button from "@mui/material/Button";
+import SearchIcon from "@mui/icons-material/Search";
 
 function Home() {
   return (
@@ -23,30 +25,41 @@ function Home() {
             ili turističkih naselja i osigurajte svoj savršeni odmor po mjeri
           </p>
         </div>
-        <div className="search">
-          <div className="lokacija">
-            <p>Lokacija</p>
-            <Location/>
-          </div>
+      </div>
+      <div className="search">
+        <div className="lokacija">
+          <p>Lokacija</p>
+          <Location />
+        </div>
 
-          <div className="smjestaj">
-            <p>Tip smještaja</p>
-            <Accomodation/>
-          </div>
+        <div className="smjestaj">
+          <p>Tip smještaja</p>
+          <Accomodation />
+        </div>
 
-          <div className="datum">
-            <p>Datum prijave / odjave</p>
-            <Date/>
-          </div>
+        <div className="datum">
+          <p>Datum prijave / odjave</p>
+          <Date />
+        </div>
 
-          <div className="gosti">
-            <p>Broj gostiju</p>
-            <Guests/>
-          </div>
+        <div className="gosti">
+          <p>Broj gostiju</p>
+          <Guests />
+        </div>
+
+        <div className="trazi">
+          <div className="spacer"></div>
+          <Button
+            className="search-btn"
+            variant="contained"
+            sx={{ backgroundColor: "#337589", height: 48, width: 102 }}
+            startIcon={<SearchIcon />}
+          >
+            Traži
+          </Button>
         </div>
       </div>
     </div>
   );
 }
-
 export default Home;
