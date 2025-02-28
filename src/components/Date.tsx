0deac2bfee29da07694c0a2f7983a3c65ea1b1f8
Vikthor-dev <57874994+Vikthor-dev@users.noreply.github.com>
2025-02-28 19:04:34 +0100

    Update

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -4,53 +4,43 @@ import Select, { SelectChangeEvent } from "@mui/material/Select";
 import DateIcon from "../assets/date.svg";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DateRangeCalendar } from '@mui/x-date-pickers-pro/DateRangeCalendar';
+import { DateRangeCalendar } from "@mui/x-date-pickers-pro/DateRangeCalendar";
+import Popover from "@mui/material/Popover";
 
 function Date() {
   const [lokacija, setLokacija] = React.useState("Odaberi");
-  const [isOpened, setCalendar] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
   const handleChange = (event: SelectChangeEvent<string>) => {
     setLokacija(event.target.value as string);
   };
 
-  const toggleCalendar = () => {
-    setCalendar(!isOpened);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
   };
 
-  function showHideCalendar() {
-    if (isOpened) {
-      return (
-        <div>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateRangeCalendar calendars={1} />
-          </LocalizationProvider>
-        </div>
-      );
-    }else{
-        return (
-            <div></div>
-        );
-    }
-  }
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "calendar-popover" : undefined;
 
   function renderSelectedValue(selected: string) {
-    if (selected === "Odaberi") {
-      return (
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            gap: 12,
-            height: 30,
-          }}
-        >
-          <img src={DateIcon} alt="Datum" style={{ width: 24, height: 24 }} />
-          Odaberi datum
-        </div>
-      );
-    }
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 12,
+          height: 30,
+        }}
+      >
+        <img src={DateIcon} alt="Datum" style={{ width: 24, height: 24 }} />
+        {selected === "Odaberi" ? "Odaberi datum" : selected}
+      </div>
+    );
   }
 
   return (
@@ -62,10 +52,30 @@ function Date() {
           displayEmpty
           renderValue={renderSelectedValue}
           readOnly
-          onClick={toggleCalendar}
+          onClick={handleClick}
         ></Select>
-        {showHideCalendar()}
       </FormControl>
+
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        PaperProps={{
+          style: {
+            padding: "20px",
+            width: "auto",
+          },
+        }}
+      >
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateRangeCalendar calendars={1} />
+        </LocalizationProvider>
+      </Popover>
     </div>
   );
 }
