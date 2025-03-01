a00d56f49afa3226cac70d47bb8b491229da06f6
Vikthor-dev <57874994+Vikthor-dev@users.noreply.github.com>
2025-03-01 13:32:52 +0100

    Update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,6 @@
 import "../css/Home.css";
 import Hero from "../assets/Hero.jpeg";
-import Location from "../components/Location";
-import Accomodation from "../components/Accomodation";
-import Date from "../components/Date";
-import Guests from "../components/Guests";
-import Button from "@mui/material/Button";
-import SearchIcon from "@mui/icons-material/Search";
+import Search from "../components/Search";
 
 function Home() {
   return (
@@ -26,39 +21,7 @@ function Home() {
           </p>
         </div>
       </div>
-      <div className="search">
-        <div className="lokacija">
-          <p>Lokacija</p>
-          <Location />
-        </div>
-
-        <div className="smjestaj">
-          <p>Tip smještaja</p>
-          <Accomodation />
-        </div>
-
-        <div className="datum">
-          <p>Datum prijave / odjave</p>
-          <Date />
-        </div>
-
-        <div className="gosti">
-          <p>Broj gostiju</p>
-          <Guests />
-        </div>
-
-        <div className="trazi">
-          <div className="spacer"></div>
-          <Button
-            className="search-btn"
-            variant="contained"
-            sx={{ backgroundColor: "#337589", height: 48, width: 102 }}
-            startIcon={<SearchIcon />}
-          >
-            Traži
-          </Button>
-        </div>
-      </div>
+      <Search/>
     </div>
   );
 }
