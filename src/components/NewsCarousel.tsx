1fdf1058ea64febdc8185a0d24f9c2cd775fb110
Vikthor-dev <57874994+Vikthor-dev@users.noreply.github.com>
2025-03-03 16:48:22 +0100

    Update

--- src/components/NewsCarousel.tsx
+++ src/components/NewsCarousel.tsx
@@ -1,32 +1,42 @@
-import React from "react";
 import NewsCard from "./NewsCard";
 import "../css/NewsCarousel.css";
-import ArrowIcon2 from "../assets/arrow2.svg";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 function NewsCarousel() {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2,
+    },
+    mobile: {
+      breakpoint: { max: 600, min: 0 },
+      items: 1,
+      slidesToSlide: 1,
+    },
+  };
   return (
     <div className="carousel-container">
-      <img
-      className="left-arrow"
-        src={ArrowIcon2}
-        alt="Arrow Icon"
-        style={{ width: 24, height: 24 }}
-      />
-      <div className="item">
-        <NewsCard />
-      </div>
-      <div className="item">
-        <NewsCard />
-      </div>
-      <div className="item">
-        <NewsCard />
-      </div>
-      <img
-      className="right-arrow"
-        src={ArrowIcon2}
-        alt="Arrow Icon"
-        style={{ width: 24, height: 24 }}
-      />
+      <Carousel responsive={responsive} infinite={false} draggable={false}>
+        <div>
+          <NewsCard />
+        </div>
+        <div>
+          <NewsCard />
+        </div>
+        <div>
+          <NewsCard />
+        </div>
+        <div>
+          <NewsCard />
+        </div>
+      </Carousel>
     </div>
   );
 }
