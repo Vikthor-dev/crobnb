d7f7dcbdc1a0599cca42e86a190b529a0ccf7246
Vikthor-dev <57874994+Vikthor-dev@users.noreply.github.com>
2025-03-07 00:24:59 +0100

    Update

--- src/pages/Novosti.tsx
+++ src/pages/Novosti.tsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import "../css/Novosti.css"
+import { useEffect, useState } from "react";
+import "../css/Novosti.css";
+import axios from "axios";
 
 function Novosti() {
+  interface NewsItem {
+    image: string;
+    category: string;
+    published: Date;
+    title: string;
+    text: string;
+  }
+
+  const [novosti, setNovosti] = useState<NewsItem[]>([]);
+
+  async function fetchData() {
+    try {
+      const response = await axios.get("http://localhost:8055/items/novosti");
+      let novosti = response.data.data;
+      let novosti_sliced = [...novosti].slice(0, 3);
+      setNovosti(novosti_sliced);
+      console.log("Novosti:", response.data.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      
+    <div className="n-main">
+      <p className="n-title">Novosti</p>
+      <div className="n-cards-div">
+        
+      </div>
     </div>
-  )
+  );
 }
 
-export default Novosti
+export default Novosti;
